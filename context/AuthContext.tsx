e0687508ff90fc2e5aca860454aa613a5c796e74
sumit kumar raju <sumitts317@gmail.com>
2026-01-24 20:01:46 +0530

    Update AuthContext.tsx

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -80,7 +80,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export const useAuth = () => {
+export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (context === undefined) {
     throw new Error("useAuth must be used within an AuthProvider");
